6e41b5b0d03ef089176caa0c6c7adacaead851ab
jrgallentine <76459959+jrgallentine@users.noreply.github.com>
2021-05-26 15:17:25 -0400

    contact us form initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using BizStream.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizStream.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string firstName, string lastName, string email, string message)
+        {    
+            Contact c = new Contact();
+            c.FirstName = firstName;
+            c.LastName = lastName;
+            c.Email = email;
+            c.Message = message;
+
+            string filepath = @"ContactUs.txt";
+
+            using StreamReader reader = new StreamReader(filepath);
+            string fileOutput = reader.ReadToEnd();
+            reader.Close();
+            using StreamWriter writer = new StreamWriter(filepath);
+
+
+            DateTime dt = DateTime.Now;
+            writer.WriteLine(fileOutput);
+            writer.WriteLine($"-----{dt}-----");
+            writer.WriteLine($"Name: {firstName} {lastName}");
+            writer.WriteLine($"Email: {email}");
+            writer.WriteLine($"Message: {message}");
+            writer.WriteLine();
+            writer.Close();
+
+            return View("Success",c);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
